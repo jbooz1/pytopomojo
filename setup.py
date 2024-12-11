0ef9dcebad5575f62a0b91cb6e04ff421a6c3b61
jbooz1 <jarrettbooz24@gmail.com>
2024-12-11 16:09:53 -0500

    increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pytopomojo",
-    version="0.1.1",
+    version="0.1.2",
     description="A TopoMojo API Client",
     url="https://github.com/jbooz1/pytopomojo",
     packages=find_packages(),
