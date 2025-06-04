aabf67c012df38d6e3ae71603b2faabe48db513a
jbooz1 <jarrettbooz24@gmail.com>
2025-06-04 20:21:26 +0000

    Fixes version in setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pytopomojo",
-    version="0.1.3",
+    version="0.1.4",
     description="A TopoMojo API Client",
     url="https://github.com/jbooz1/pytopomojo",
     packages=find_packages(),
