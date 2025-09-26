aa0645f5eb458ca0d3d8a0d3af6d44359055157c
jbooz1 <jarrettbooz24@gmail.com>
2025-09-26 18:27:59 +0000

    Adds devcontainer

---
+++ .devcontainer/postcreate.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -euo pipefail
+
+# Show git dirty status in zsh prompt
+git config devcontainers-theme.show-dirty 1
+
+# oh-my-zsh plugins
+sed -i 's/^\(\s*plugins=(.*\)\s*)/\1 python pyenv pip git)/' $HOME/.zshrc
+
+# Setup pure prompt
+sudo npm install --global pure-prompt
+sed -i "s|^ZSH_THEME=.*|ZSH_THEME=\"\"\n\nFPATH=$(npm root -g)/pure-prompt/functions:\$FPATH|" $HOME/.zshrc
+
+cat >>$HOME/.zshrc <<'EOF'
+
+# Pure prompt
+autoload -U promptinit; promptinit
+prompt pure
+
+EOF
+
+# install pip packages
+pip install --no-cache-dir -e .
