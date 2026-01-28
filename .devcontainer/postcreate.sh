5747572e048622595f1af7ad873922f49317c5f5
Jarrett Booz <jarrettbooz24@gmail.com>
2026-01-28 17:32:28 +0000

    Simplify dev container

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -7,18 +7,5 @@ git config devcontainers-theme.show-dirty 1
 # oh-my-zsh plugins
 sed -i 's/^\(\s*plugins=(.*\)\s*)/\1 python pyenv pip)/' $HOME/.zshrc
 
-# Setup pure prompt
-# Using npm prefix /usr/local to avoid conflicting with system-managed node_modules
-sudo npm install --global --prefix /usr/local pure-prompt
-sed -i "s|^ZSH_THEME=.*|ZSH_THEME=\"\"\n\nFPATH=$(npm root -g)/pure-prompt/functions:\$FPATH|" $HOME/.zshrc
-
-cat >>$HOME/.zshrc <<'EOF'
-
-# Pure prompt
-autoload -U promptinit; promptinit
-prompt pure
-
-EOF
-
 # install project in editable mode using system Python
 python -m pip install --no-cache-dir -e .
