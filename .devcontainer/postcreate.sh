080d9746a1b15539b241e1cab162de37794dd4cb
jbooz1 <jarrettbooz24@gmail.com>
2026-03-30 08:52:43 -0400

    Adds claude to dev container (#14)

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 # Show git dirty status in zsh prompt
 git config devcontainers-theme.show-dirty 1
 
+sudo chown -R $(whoami): /home/vscode/.claude
+
 # oh-my-zsh plugins
 sed -i 's/^\(\s*plugins=(.*\)\s*)/\1 python pyenv pip)/' $HOME/.zshrc
 
